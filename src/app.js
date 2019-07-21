229502915283f5577105bcd03db36a29cef995f7
Robin Khiv <robinkhiv@gmail.com>
2019-07-21 14:24:57 -0700

    graphql working on /graphql endpoint

--- src/app.js
+++ src/app.js
@@ -6,8 +6,22 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -16,8 +30,11 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
-app.get('/', (req, res) => {
-  res.send('Hello, world!');});
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
